db18380dc16c59dc10f07a6364b1c814ae7a6a87
TVanmeerhaeghe <vanmeerhaegheteo@gmail.com>
2025-03-13 12:21:17 +0100

    feat: :sparkles: Added division function

--- src/utils/calculator.js
+++ src/utils/calculator.js
@@ -10,4 +10,11 @@ export class Calculator {
     multiply(a, b) {
         return a * b;
     }
+
+    divide(a, b) {
+        if (b === 0) {
+            return null;
+        }
+        return a / b;
+    }
 }
