471c1ce1711d4556f911056c48be354dc46c9ed6
TVanmeerhaeghe <vanmeerhaegheteo@gmail.com>
2025-03-13 11:09:23 +0100

    test: :sparkles: Added function and test to add operation in history

--- src/utils/calculator.js
+++ src/utils/calculator.js
@@ -9,3 +9,32 @@ export function subtract(a, b) {
 export function multiply(a, b) {
     return a * b;
 }
+
+export class Calculator {
+    constructor() {
+        this.history = [];
+    }
+
+    add(a, b) {
+        const result = a + b;
+        this._addToHistory(a, b, "+", result);
+        return result;
+    }
+
+    subtract(a, b) {
+        const result = a - b;
+        this._addToHistory(a, b, "-", result);
+        return result;
+    }
+
+    multiply(a, b) {
+        const result = a * b;
+        this._addToHistory(a, b, "*", result);
+        return result;
+    }
+
+    _addToHistory(a, b, operator, result) {
+        this.history.push(`${a} ${operator} ${b} = ${result}`);
+    }
+
+}
--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -1,6 +1,7 @@
 import { add } from '../../src/utils/calculator';
 import { subtract } from '../../src/utils/calculator';
 import { multiply } from '../../src/utils/calculator';
+import { Calculator } from '../../src/utils/calculator';
 
 describe("Addition", () => {
     test("addition de 2 et 3 retourne 5", () => {
@@ -8,7 +9,7 @@ describe("Addition", () => {
     });
 
     test("addition de nombres négatifs fonctionne", () => {
-        expect(add(-2, -3)).toBe(-5);
+        expect(add(-22, -5)).toBe(-27);
     });
 });
 
@@ -18,7 +19,7 @@ describe("Soustraction", () => {
     });
 
     test("soustraction de nombres négatifs fonctionne", () => {
-        expect(subtract(-5, -3)).toBe(-2);
+        expect(subtract(-58, -20)).toBe(-38);
     });
 });
 
@@ -28,6 +29,29 @@ describe("Multiplication", () => {
     });
 
     test("multiplication par 0 retourne 0", () => {
-        expect(multiply(5, 0)).toBe(0);
+        expect(multiply(88, 0)).toBe(0);
+    });
+});
+
+describe("Historique", () => {
+    let calc;
+
+    beforeEach(() => {
+        calc = new Calculator();
+    });
+
+    test("ajoute une addition à l'historique", () => {
+        calc.add(2, 3);
+        expect(calc.history).toEqual(["2 + 3 = 5"]);
+    });
+
+    test("ajoute une soustraction à l'historique", () => {
+        calc.subtract(10, 4);
+        expect(calc.history).toEqual(["10 - 4 = 6"]);
+    });
+
+    test("ajoute une multiplication à l'historique", () => {
+        calc.multiply(3, 5);
+        expect(calc.history).toEqual(["3 * 5 = 15"]);
     });
 });
