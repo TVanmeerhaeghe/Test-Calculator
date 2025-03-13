e22eb8b038cf49c59111ff0a921698214289d274
TVanmeerhaeghe <vanmeerhaegheteo@gmail.com>
2025-03-13 10:13:58 +0100

    test: :white_check_mark: Added test for Addition

---
+++ tests/unit/calculator.spec.js
@@ -0,0 +1,11 @@
+import { add } from '../src/utils/calculator';
+
+describe("Addition", () => {
+    test("addition de 2 et 3 retourne 5", () => {
+        expect(add(2, 3)).toBe(5);
+    });
+
+    test("addition de nombres négatifs fonctionne", () => {
+        expect(add(-2, -3)).toBe(-5);
+    });
+});
