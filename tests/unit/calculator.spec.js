28a3065de9f4e5d2be955f68fe269cc06d7253ad
TVanmeerhaeghe <vanmeerhaegheteo@gmail.com>
2025-03-13 10:41:43 +0100

    test: :white_check_mark: Added test for multiplication

--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -1,5 +1,6 @@
 import { add } from '../../src/utils/calculator';
 import { subtract } from '../../src/utils/calculator';
+import { multiply } from '../../src/utils/calculator';
 
 describe("Addition", () => {
     test("addition de 2 et 3 retourne 5", () => {
@@ -18,5 +19,15 @@ describe("Soustraction", () => {
 
     test("soustraction de nombres négatifs fonctionne", () => {
         expect(subtract(-5, -3)).toBe(-2);
+    });
+});
+
+describe("Multiplication", () => {
+    test("multiplication de 2 et 3 retourne 6", () => {
+        expect(multiply(2, 3)).toBe(6);
+    });
+
+    test("multiplication par 0 retourne 0", () => {
+        expect(multiply(5, 0)).toBe(0);
     });
 });
