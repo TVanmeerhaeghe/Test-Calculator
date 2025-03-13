10e3b1c7c2c93e801acec684fd2733d4c87f0d30
TVanmeerhaeghe <vanmeerhaegheteo@gmail.com>
2025-03-13 10:25:38 +0100

    test: :white_check_mark: Added test for Soustraction

--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -1,4 +1,5 @@
 import { add } from '../../src/utils/calculator';
+import { subtract } from '../../src/utils/calculator';
 
 describe("Addition", () => {
     test("addition de 2 et 3 retourne 5", () => {
@@ -7,5 +8,15 @@ describe("Addition", () => {
 
     test("addition de nombres négatifs fonctionne", () => {
         expect(add(-2, -3)).toBe(-5);
+    });
+});
+
+describe("Soustraction", () => {
+    test("soustraction de 5 et 3 retourne 2", () => {
+        expect(subtract(5, 3)).toBe(2);
+    });
+
+    test("soustraction de nombres négatifs fonctionne", () => {
+        expect(subtract(-5, -3)).toBe(-2);
     });
 });
